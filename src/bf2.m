e3aa206c33d4b08be624059148713b42ec0ded95
Filip Żarnowiec <filipzarnowiec@gmail.com>
2021-04-24 04:01:41 +0200

    version 2.0

--- src/bf2.m
+++ src/bf2.m
@@ -1,39 +1,80 @@
-condition = "rewards";
-event = [
-    "plan_UCSp_porn",...
-    "plan_UCSp_cash",...
-    "unpl_UCSp_porn",...
-    "unpl_UCSp_cash"
-    ];
-figure(10)
-hold on
-title("BF " + condition + " - UCSp");
-xline(0, '--r');
-ylim([-0.05,0.05]);
+eventsRewards = containers.Map(...
+    ["UCSp porn", "UCSp cash", "No UCSp", "No UCSp cash", "No UCSp_porn"],...
+    [{["plan_UCSp_porn", "unpl_UCSp_porn"]},...
+    {["plan_UCSp_cash", "unpl_UCSp_cash"]},...
+    {["NoUCsm", ""]},...
+    {["plan_No_UCSp_cash","unpl_No_UCSp_cash"]},...
+    {["plan_No_UCSp_porn", "unpl_No_UCSp_porn"]}]);
+eventsCues = containers.Map(...
+    ["CS Minus", "win CS Cash", "lose CS Cash", "lose CS Porn", "win CS Porn"],...
+    [
+    {"CS_Minus"},...
+    {"to_win_CS_Plus_Cash"},...
+    {"to_lose_CS_Plus_Cash"},...
+    {"to_lose_CS_Plus_Porn"},...
+    {"to_win_CS_Plus_Porn"}
+    ]);
 
-skipped = 0;
-for i = 1 : length(ALLEEG)
-    eeg = ALLEEG(i);
-    acc = [];
-    
-    if(condition ~= eeg.condition)
-        skipped = skipped + 1;
-        continue;
+cd("../plots/butterfly_pure")
+
+for i = 1 : 3
+    %i stands for group
+    for key = keys(eventsRewards)
+        drawButterFly(alleeg2, key{1}, eventsRewards(key{1}), i, "rewards");
     end
     
+        
+    for key = keys(eventsCues)
+        drawButterFly(alleeg2, key{1}, eventsCues(key{1}), i, "cues")
+    end    
+end
+
+
+
+%%
+function drawButterFly(ALLEEG, eventType, events, group, condition)
+acc = [];
+for i = 1 : length(ALLEEG)
+    eeg = ALLEEG(i);
+    if(condition ~= eeg.condition)
+        continue;
+    end
+    if(group ~= eeg.group)
+        continue;
+    end
+
     for j = 1 : length(eeg.epoch)
-        if(sum(event == eeg.epoch(j).eventtype) == 0)
+        if(sum(events == eeg.epoch(j).eventtype) == 0)
             continue;
         end
         
-        betterEye = 1;
-        if(sum(isnan(eeg.data(1, :, j))) > sum(isnan(eeg.data(2, :, j))))
-        betterEye = 2;
-        end
-        acc = [acc; eeg.data(betterEye, :, j)]; %#ok<AGROW>
+        acc = [acc; eeg.data(1, :, j)]; %#ok<AGROW>
     end
-
-    plot(eeg.times, mean(acc));
 end
+[n,~] = size(acc);
+tit = "" + eventType + " | group: " + group;
+txt = "epochs :" + n;
 
-hold off
+figure(8);
+clf;
+hold on;
+for i = 1 : n
+    plot(eeg.times, acc(i,:));
+end
+hold off;
+
+title(tit);
+xline(0, '--r');
+ylim([-0.05,0.05]);
+text(0, 0.04, txt);
+saveas(gcf, "" + eventType + "_" + group + ".jpg");
+
+
+plot(eeg.times, mean(acc, 'omitnan'));
+title(append(tit, " -> MEAN"));
+xline(0, '--r');
+ylim([-0.05,0.05]);
+text(0, 0.04, txt);
+saveas(gcf, "" + eventType + "_" + group + "_MEAN" + ".jpg");
+
+end
--- src/pupillometry.m
+++ src/pupillometry.m
@@ -1,20 +1,17 @@
 %% const variables
-MEAN_RANGE = 5;
-w2h_UPPER_THRESHOLD = 1.3;
-w2h_LOWER_THRESHOLD = 0.7;
-MIN_INTERVAL_DISTANCE = 13;
-INTERPOLATION_OFFSET = 3;
-START_RANGE_REWARDS = -1;
-END_RANGE_REWARDS = 2.5;
-START_RANGE_CLUES = -1;
-END_RANGE_CLUES = 5;
 summary = [];
+ 
 % conditions
 clues = {'CS_Minus' 'to_win_CS_Plus_Cash' 'to_lose_CS_Plus_Cash' 'to_lose_CS_Plus_Porn' 'to_win_CS_Plus_Porn'};
+START_RANGE_CLUES = -1;
+END_RANGE_CLUES = 2;
+
 rewards = {'NoUCsm' 'plan_No_UCSp_cash' 'plan_No_UCSp_porn' 'plan_UCSp_porn' 'plan_UCSp_cash' 'unpl_No_UCSp_porn' 'unpl_No_UCSp_cash' 'unpl_UCSp_cash' 'unpl_UCSp_porn' };
+START_RANGE_REWARDS = -1;
+END_RANGE_REWARDS = 2;
 
 %% map defing group of a subject (control or study)
-csv = readtable('../resources/subjects.csv');
+csv = readtable('../resources/subjects.csv', 'Format', '%s%u');
 subjects = string(csv.subject_id);
 subjectsMap = containers.Map(subjects, csv.group_id);
 
@@ -45,39 +42,44 @@ for k = 1 : length(sub_list)
     underscoresIndexes = strfind(currentSubject,'_');
     currentSubject = currentSubject(1 : underscoresIndexes(1) - 1);
     
-%     if(~strcmp(currentSubject, '11210595'))
+%     if(~strcmp(currentSubject, '91218349'))
 %        continue;
 %     end
     try
         try
-            eeg = loadEegSet(sub_list(k), MEAN_RANGE, w2h_UPPER_THRESHOLD,...
-                w2h_LOWER_THRESHOLD, MIN_INTERVAL_DISTANCE, INTERPOLATION_OFFSET);
+            [eeg, w2h_A, w2h_B] = loadEegSet(sub_list(k));
+            
+            eeg = eeg_checkset(eeg);
         catch e
             summary = [summary; "cannot load : " + sub_list(k).name];
             continue;
         end
+        
+        try
+            eeg.group = subjectsMap(currentSubject);
+        catch e
+            summary = [summary; "unable to define group of subject : " + currentSubject];
+            continue;
+        end
 
-        eeg = eeg_checkset( eeg );
-
+        eeg.data(1,:) = filter_w2h(eeg.data(1,:), w2h_A);
+        eeg.data(2,:) = filter_w2h(eeg.data(2,:), w2h_B);
+        eeg = pop_eegfiltnew(eeg, [],10,[],0,[],0);
+        
         eeg1 = eeg;
-        eeg1 = pop_eegfiltnew(eeg1, [],10,[],0,[],0);
+        
         eeg1 = pop_epoch( eeg1, clues, [START_RANGE_CLUES  END_RANGE_CLUES], 'epochinfo', 'yes');
         eeg1.setname = eeg1.setname(1: end - 7);
-        eeg1.setname = strcat(eeg1.setname, '_clues');
-        eeg1.condition = 'clues';
-        %eeg1 = addEpochInfo(eeg1);
-        eeg1 = eeg_checkset(eeg1);
+        eeg1.setname = strcat(eeg1.setname, '_cues');
+        eeg1.condition = 'cues';
         eeg1 = pop_rmbase( eeg1, [-300    0]); %rmbase_qb zmienia na procentowy baseline (divisive as opposed to substractive)
         eeg1 = eeg_checkset( eeg1 );
         [ALLEEG, EEG, CURRENTSET] = eeg_store(ALLEEG, eeg1);
 
-        eeg = pop_eegfiltnew(eeg, [],10,[],0,[],0);
         eeg = pop_epoch( eeg, rewards, [START_RANGE_REWARDS  END_RANGE_REWARDS], 'epochinfo', 'yes');
         eeg.setname = eeg.setname(1: end - 7);
         eeg.setname = strcat(eeg.setname, '_rewards');
         eeg.condition = 'rewards';
-        %eeg = addEpochInfo(eeg);
-        eeg = eeg_checkset(eeg);
         eeg = pop_rmbase( eeg, [-300    0]); %rmbase_qb zmienia na procentowy baseline (divisive as opposed to substractive)
         eeg = eeg_checkset( eeg );
         [ALLEEG, EEG, CURRENTSET] = eeg_store(ALLEEG, eeg);
@@ -109,262 +111,3 @@ for k = 1 : length(sub_list)
 end
 
 eeglab redraw
-%%
-%WARNING: function works with the assumption that data(3,:) corresond to
-%intervals_A and 4 correspond to intervals_B
-function eeg = addEpochInfo(eeg)
-    %%eeg.epoch.intervals_A
-    for i = 1 : length(eeg.epoch)
-        j = 1;
-        eeg.epoch(i).intervals_A = [];
-        while(j <= eeg.pnts)
-            if(eeg.data(3, j, i) ~= 0)
-                startJ = j;
-                while(eeg.data(3, j, i) ~= 0)
-                    if(j >= eeg.pnts)
-                        break;
-                    end
-                    j = j + 1;
-                end
-                eeg.epoch(i).intervals_A = [eeg.epoch(i).intervals_A; startJ, j];
-            end
-            j = j + 1;
-        end
-    end
-    
-    %%eeg.epoch.intervals_B
-    for i = 1 : length(eeg.epoch)
-        j = 1;
-        eeg.epoch(i).intervals_B = [];
-        while(j <= eeg.pnts)
-            if(eeg.data(4, j, i) == 1)
-                startJ = j;
-                while(eeg.data(4, j, i) == 1)
-                    if(j >= eeg.pnts)
-                        break;
-                    end
-                    j = j + 1;
-                end
-                eeg.epoch(i).intervals_B = [eeg.epoch(i).intervals_B; startJ, j];
-            end
-            j = j + 1;
-        end
-    end
-    
-    %%percentage of interpolated data    
-    for i = 1 : length(eeg.epoch)
-        eeg.epoch(i).noisePercentage_A = 100 * sum(eeg.data(3,:,i))/size(eeg.data, 2);
-        eeg.epoch(i).noisePercentage_B = 100 * sum(eeg.data(4,:,i))/size(eeg.data, 2);
-    end
-    
-    %%matlab is just stupid
-    eeg.data(3,:,:) = [];
-    eeg.data(3,:,:) = [];
-end
-%%
-function tmpIntervalsData = writeTempIntervalData(intervals, pnts)
-    tmpIntervalsData = zeros(1, pnts);
-    if (isempty(intervals))
-        return;
-    end
-    if(intervals(1,1) < 1)
-        intervals(1,1) = 1;
-    end
-    
-    [rows, ~] = size(intervals);
-    for i = 1 : rows-1
-        tmpIntervalsData(intervals(i, 1) : intervals(i, 2)) = 1;
-    end
-end
-%%
-function interpolatedData = interpolateIntervals(data, intervals, MEAN_RANGE)
-    interpolatedData = [];
-    counter = 1;
-    [intervalsRows,~] = size(intervals);
-    stop = -1;
-    if(~isempty(intervals))
-        stop = intervals(counter,1);       
-        if (stop <= MEAN_RANGE + 1)
-            stop = MEAN_RANGE + 1 + 1;
-        end
-        if(intervals(end, 2) >= length(data) - MEAN_RANGE - 1)
-            intervals(end,2) = length(data) - MEAN_RANGE - 1;
-        end
-    end
-
-    i = 1;
-    while(i <= length(data))
-        if(i == stop)
-            interpolatedData = interpolatedData(1:end);
-            interpolatedData = [interpolatedData,...
-                linspace(mean(data(stop - MEAN_RANGE - 1 : stop - 1)),...
-                         mean(data(intervals(counter, 2) + 1 : intervals(counter, 2) + 1 + MEAN_RANGE)), ...
-                         (intervals(counter, 2) - stop + 1))]; %#ok<AGROW>
-            
-           i = intervals(counter,2) + 1; 
-           counter = counter + 1;
-           if(counter < intervalsRows)
-                stop = intervals(counter,1);
-           end
-        end
-        interpolatedData(i) = data(i);  %#ok<AGROW>
-        i = i + 1;
-    end
-end
-%%
-function intervals = getIntervals(M, w2h_UPPER_THRESHOLD, w2h_LOWER_THRESHOLD, MIN_INTERVAL_DISTANCE, INTERPOLATION_OFFSET)
-    intervals = [];
-    tmp = NaN;
-    for i = 1 : length(M)
-        if(M(i) > w2h_UPPER_THRESHOLD || M(i) < w2h_LOWER_THRESHOLD)
-            if(isnan(tmp))
-                tmp = i - INTERPOLATION_OFFSET;
-            end
-        else
-            if(~isnan(tmp))
-                intervals = [intervals; tmp, i + INTERPOLATION_OFFSET]; %#ok<AGROW>
-                tmp = NaN;
-            end
-        end
-    end
-    
-    if(isnan(tmp))
-       intervals = [intervals; tmp, length(M)]; %#ok<AGROW>
-    end    
-    %% suming close intervals
-    [rows, ~] = size(intervals);
-    garbbage = [];
-    for i = 1 : rows-1
-        if(intervals(i, 2) + MIN_INTERVAL_DISTANCE > intervals(i+1, 1))
-            intervals(i+1, 1) = intervals(i, 1);
-            garbbage = [garbbage; i];
-        end
-    end
-    if(isempty(intervals) || rows < 2)
-        return
-    end
-    intervals(rows, 1) = intervals(rows-1,1);
-    garbbage = [garbbage; rows-1];
-    
-    intervals(garbbage,:) = [];
-    
-end
-
-%%
-function data = interpolateData(rawData, w2h, w2h_UPPER_THRESHOLD, w2h_LOWER_THRESHOLD)
-    data = NaN(1, length(rawData));
-    for i = 1 : length(data)
-        if(w2h(i) < w2h_UPPER_THRESHOLD && w2h(i) > w2h_LOWER_THRESHOLD)
-            data(i) = rawData(i);
-        end
-    end
-end
-%%
-function eeg = loadEegSet(file, MEAN_RANGE, w2h_UPPER_THRESHOLD, w2h_LOWER_THRESHOLD, MIN_INTERVAL_DISTANCE, INTERPOLATION_OFFSET)
-    DEBUG_PLOT = false;
-
-    eeg = eeg_emptyset();
-    
-    %% loading some study infromation
-    eeg.setname = file.name(1 : end-4);
-    eeg.trials = 1;
-    eeg.srate = 60;
-    underscoresIndexes = strfind(eeg.setname,'_');
-    eeg.subject = file.name(1 : underscoresIndexes(1) - 1);
-    
-    underscoresData = eeg.setname((underscoresIndexes(1) + 1) : end);
-    
-    if(length(underscoresData) == 1)
-        eeg.group = 1;
-        eeg.session = str2num(underscoresData(1));
-    elseif (length(underscoresData) == 3)
-         eeg.group = 2;
-         eeg.session = str2num(underscoresData(3));
-    else
-        eeg.group = 0;
-        eeg.session = 0;
-    end
-    
-    %% loading raw data
-    filename = [file.folder '/' file.name];
-    formatSpec = '%f%f%s%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%s%[^\n\r]';
-    fileID = fopen(filename,'r');
-    dataArray = textscan(fopen(filename,'r'), formatSpec,...
-        'Delimiter', '\t', 'TextType', 'string', 'EmptyValue', NaN,...
-        'HeaderLines' ,44-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
-    fclose(fileID);
-    t = table(dataArray{1:end-1}, 'VariableNames', {'ev_index','TotalTime','DeltaTime','A_X_Gaze','A_Y_Gaze','VarName6','VarName7','VarName8','A_PupilWidth',...
-        'A_PupilHeight','VarName11','VarName12','VarName13','VarName14','B_X_Gaze','B_Y_Gaze','VarName17','VarName18','VarName19','B_PupilWidth','B_PupilHeight',...
-        'VarName22','VarName23','VarName24','VarName25','VarName26','Count','Markers'});
-    clearvars formatSpec fileID dataArray ;
-
-    t = removevars(t, {'VarName6','VarName7','VarName8','VarName11','VarName12','VarName13','VarName14','VarName17','VarName18','VarName19','VarName22',...
-        'VarName23','VarName24','VarName25','VarName26','A_X_Gaze','A_Y_Gaze','B_X_Gaze','B_Y_Gaze'});
-
-    s = table2struct(t);
-    clear t    
-    
-    %% loading events
-    
-    events = [];
-    for i = 1:length(s)
-        if s(i).ev_index == 12
-            events = [events, s(i)]; %#ok<AGROW>
-        end
-    end
-    %deleting marker record
-    s = s(~isnan([s.A_PupilWidth]));   
-
-    %% calculating PupilDiam and w2h
-    w2h_B = zeros(1,length(s));
-    w2h_A = zeros(1,length(s));
-    for ii = 1:length(s)
-
-        s(ii).B_PupilDiam = mean([s(ii).B_PupilHeight s(ii).B_PupilWidth]);
-        s(ii).A_PupilDiam = mean([s(ii).A_PupilHeight s(ii).A_PupilWidth]);
-        w2h_B(ii) = (s(ii).B_PupilWidth / s(ii).B_PupilHeight);
-        w2h_A(ii) = (s(ii).A_PupilWidth / s(ii).A_PupilHeight);
-
-    end  
-    
-    %% adding event info
-    gg=1;
-    eeg.event(1:end) = [];
-    for ii = 1:length(events)
-        if contains(events(ii).DeltaTime, 'CS_') || contains(events(ii).DeltaTime, 'UC')
-            eeg.event(gg).latency = (events(ii).TotalTime*1000)/16.66;
-            eeg.event(gg).type = events(ii).DeltaTime;
-            eeg.event(gg).code = events(ii).DeltaTime;
-            eeg.event(gg).bvmknum = ii;
-            eeg.event(gg).duration = 1;
-            eeg.event(gg).channel = 0;
-            eeg.event(gg).bvtime = 1;
-            gg=gg+1;
-        end
-    end
-
-    %% setting data   
-    eeg.pnts = length([s(:).TotalTime]);
-    eeg.times(1,:) = [s(:).TotalTime];
-    
-    eeg.data(1,:) = interpolateData([s(:).A_PupilDiam], w2h_A, w2h_UPPER_THRESHOLD, w2h_LOWER_THRESHOLD);
-    eeg.data(2,:) = interpolateData([s(:).B_PupilDiam], w2h_B, w2h_UPPER_THRESHOLD, w2h_LOWER_THRESHOLD);
-    
-    %eeg.intervals_A = getIntervals(w2h_A, w2h_UPPER_THRESHOLD, w2h_LOWER_THRESHOLD, MIN_INTERVAL_DISTANCE, INTERPOLATION_OFFSET);
-    %eeg.intervals_B = getIntervals(w2h_B, w2h_UPPER_THRESHOLD, w2h_LOWER_THRESHOLD, MIN_INTERVAL_DISTANCE, INTERPOLATION_OFFSET);
-     
-    %eeg.data(1,:) = interpolateIntervals([s(:).A_PupilDiam], eeg.intervals_A, MEAN_RANGE);
-    %eeg.data(2,:) = interpolateIntervals([s(:).B_PupilDiam], eeg.intervals_B, MEAN_RANGE);
-    %chan 3 and 4 temp (will be removed after cuting into epoch)
-    %eeg.data(3,:) = writeTempIntervalData(eeg.intervals_A, eeg.pnts);
-    %eeg.data(4,:) = writeTempIntervalData(eeg.intervals_B, eeg.pnts);
-    
-    if(DEBUG_PLOT)
-        figure(11);
-        plot([s(:).TotalTime], [s(:).A_PupilDiam], [s(:).TotalTime], eeg.data(1,:));
-    end
-    
-    clear s;
-    
-
-end
